fc074c3add93cbdb84b278180e30bb5b93dd74ea
haeunko <hekoh99@naver.com>
2022-02-13 13:00:06 +0900

    prisma set

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
+  type Movie {
+    id: Int
+    title: String
+    year: Int
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "sweet!",
+    movies: () => [],
+    movie: () => ({ title: "120bpm", year: 2018 }),
   },
 };
 
