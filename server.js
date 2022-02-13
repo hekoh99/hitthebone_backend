c8f4bfa98e97e5c35291721af95d82603e7c3cf9
haeunko <hekoh99@naver.com>
2022-02-13 18:25:32 +0900

    db

--- server.js
+++ server.js
@@ -1,50 +1,9 @@
-import { ApolloServer, gql } from "apollo-server";
-import { PrismaClient } from "@prisma/client";
+require("dotenv").config();
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
 
-const prisma = new PrismaClient();
-
-const typeDefs = gql`
-  type Movie {
-    id: Int!
-    title: String!
-    year: Int!
-    createdAt: String!
-  }
-  type Query {
-    movies: [Movie]
-    movie(id: Int!): Movie
-  }
-  type Mutation {
-    createMovie(title: String!, year: Int!): Movie
-    deleteMovie(id: Int!): Movie
-  }
-`;
-
-const resolvers = {
-  Query: {
-    movies: () => prisma.movie.findMany(),
-    movie: (_, { id }) =>
-      prisma.movie.findUnique({
-        where: { id },
-      }),
-  },
-  Mutation: {
-    createMovie: (_, { title, year }) =>
-      prisma.movie.create({
-        data: {
-          title,
-          year,
-        },
-      }),
-    deleteMovie: (_, { id }) =>
-      prisma.movie.delete({
-        where: { id },
-      }),
-  },
-};
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
+const server = new ApolloServer({ schema });
+const PORT = process.env.SERVER_PORT;
 server
-  .listen()
-  .then(() => console.log("server is running http://localhost:4000"));
+  .listen(PORT)
+  .then(() => console.log(`🚀 server is running http://localhost:${PORT}`));
