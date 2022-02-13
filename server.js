957a9a888860e1c8d36d447ddb94ad22d0ff1e8e
haeunko <hekoh99@naver.com>
2022-02-13 23:10:10 +0900

    port

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 
-const server = new ApolloServer({ schema });
 const PORT = process.env.SERVER_PORT;
+const server = new ApolloServer({ schema });
+
 server
   .listen(PORT)
   .then(() => console.log(`🚀 server is running http://localhost:${PORT}`));
