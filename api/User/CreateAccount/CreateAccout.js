2e24b030dcef31b419d8bed43bce69fa89662d42
haeunko <hekoh99@naver.com>
2022-02-15 22:17:56 +0900

    prevent duplicate

--- api/User/CreateAccount/CreateAccout.js
+++ api/User/CreateAccount/CreateAccout.js
@@ -1,14 +1,27 @@
+import bcrypt from "bcrypt";
+
 export default {
   Mutation: {
     CreateAccount: async (_, args, context) => {
-      const { email, password, nickname } = args;
-      return await context.prisma.User.create({
-        data: {
-          email,
-          nickname,
-          password,
-        },
-      });
+      try {
+        const { email, password, nickname } = args;
+        const isExist = await context.prisma.User.findFirst({
+          where: {
+            OR: [{ email }, { nickname }],
+          },
+        });
+        if (isExist) throw Error("duplicated email or nickname");
+        const hashedPwd = await bcrypt.hash(password, 10);
+        return context.prisma.User.create({
+          data: {
+            email,
+            nickname,
+            password: hashedPwd,
+          },
+        });
+      } catch (e) {
+        return e;
+      }
     },
   },
 };
