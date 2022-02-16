4fd0c57b39cffd11c28eac379e8a4c1decea98b8
haeunko <hekoh99@naver.com>
2022-02-16 21:49:10 +0900

    edit profile/redefine type

--- api/Login/UserLogin/UserLogin.js
+++ api/Login/UserLogin/UserLogin.js
@@ -12,19 +12,17 @@ export default {
       });
       if (!dbUser)
         return {
-          status: false,
-          errorMsg: `no user called ${nickname}`,
+          result: { status: false, errorMsg: `no user called ${nickname}` },
         };
       const status = await bcrypt.compare(password, dbUser.password);
       if (!status)
         return {
-          status,
-          errorMsg: `wrong password`,
+          result: { status: false, errorMsg: `wrong password` },
         };
       /* to set expiration date, send {expiresIn} or {notBefore} on third parameter */
       const token = await jwt.sign({ id: dbUser.id }, process.env.SECRET_KEY);
       return {
-        status: true,
+        result: { status: true },
         token,
       };
     },
---
+++ api/User/EditProfile/EditProfile.js
@@ -0,0 +1,5 @@
+export default {
+  Mutation: {
+    EditProfile: (_, args, context) => {},
+  },
+};
