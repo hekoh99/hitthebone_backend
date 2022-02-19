f756dd79c1b5b4f68b12a565605a19360ac52b8f
haeunko <hekoh99@naver.com>
2022-02-19 16:02:49 +0900

    edit profile

--- api/User/EditProfile/EditProfile.js
+++ api/User/EditProfile/EditProfile.js
@@ -1,7 +1,23 @@
+import bcrypt from "bcrypt";
+
 export default {
   Mutation: {
     EditProfile: async (_, args, context) => {
-      return context.prisma.Post.findMany({ where: {} });
+      const { nickname, email, password } = args;
+      let hashedPwd = null;
+      if (password) hashedPwd = await bcrypt.hash(password, 10);
+      const updatedUser = await context.prisma.User.update({
+        where: {
+          nickname: "admin", // change with authentication info later
+        },
+        data: {
+          nickname,
+          email,
+          ...(hashedPwd && { password: hashedPwd }),
+        },
+      });
+      if (updatedUser.id) return { status: true };
+      return { status: false, errorMsg: "cannot update profile" };
     },
   },
 };
