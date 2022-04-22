79ec3e207c3cc4be13ca29c81c35faa15aa45133
haeunko <hekoh99@naver.com>
2022-04-22 13:55:00 +0900

    resolver wrapped

--- api/User/EditProfile/EditProfile.js
+++ api/User/EditProfile/EditProfile.js
@@ -1,24 +1,26 @@
 import bcrypt from "bcrypt";
 import { authResolver } from "../../../utils/authResolver";
 
+const editFunc = async (_, args, context) => {
+  const { nickname, email, password } = args;
+  let hashedPwd = null;
+  if (password) hashedPwd = await bcrypt.hash(password, 10);
+  const updatedUser = await context.prisma.User.update({
+    where: {
+      id: context.user.id,
+    },
+    data: {
+      nickname,
+      email,
+      ...(hashedPwd && { password: hashedPwd }),
+    },
+  });
+  if (updatedUser.id) return { status: true };
+  return { status: false, errorMsg: "cannot update profile" };
+};
+
 export default {
   Mutation: {
-    EditProfile: authResolver(async (_, args, context) => {
-      const { nickname, email, password } = args;
-      let hashedPwd = null;
-      if (password) hashedPwd = await bcrypt.hash(password, 10);
-      const updatedUser = await context.prisma.User.update({
-        where: {
-          id: context.user.id,
-        },
-        data: {
-          nickname,
-          email,
-          ...(hashedPwd && { password: hashedPwd }),
-        },
-      });
-      if (updatedUser.id) return { status: true };
-      return { status: false, errorMsg: "cannot update profile" };
-    }),
+    EditProfile: authResolver(editFunc),
   },
 };
